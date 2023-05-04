e9cbc973f9def2592904eae517139782c980ae07
MohamedRezq <mrezq.dev@gmail.com>
2023-05-04 19:33:02 +0300

    fixed-2

--- components/molecules/FormLogin/FormLogin.tsx
+++ components/molecules/FormLogin/FormLogin.tsx
@@ -1,6 +1,7 @@
 import BasicInput from "@/components/atoms/Input/TextInput";
-import {TbLogout} from 'react-icons/tb';
+import { TbLogout } from "react-icons/tb";
 import React, { ReactNode } from "react";
+import Link from "next/link";
 
 interface FormLoginProps {}
 const FormLogin = () => {
@@ -14,10 +15,12 @@ const FormLogin = () => {
       >
         Forgot Password?
       </a>
-      <button className="w-full mt-5 text-white text-xl bg-hippiegreen rounded-3xl py-5 flex justify-center items-center gap-x-4 bg-opacity-90 hover:bg-opacity-100">
-        <TbLogout />
-        Sign In
-      </button>
+      <Link href="/welcome/select-saas">
+        <button className="w-full mt-5 text-white text-xl bg-hippiegreen rounded-3xl py-5 flex justify-center items-center gap-x-4 bg-opacity-90 hover:bg-opacity-100">
+          <TbLogout />
+          Sign In
+        </button>
+      </Link>
     </form>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import FormLogin from '@/components/molecules/FormLogin/FormLogin'
+import React from "react";
+import {BsArrowRight} from 'react-icons/bs';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const HelloPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
-      <FormLogin />
+    <main className="flex max-w-md m-auto min-h-screen flex-col items-center justify-between py-16 h-full">
+      <div></div>
+      <div className="text-7xl text-emperor">Hello !</div>
+      <div className="text-lg text-silverchalice py-10">www.alphasaas.io</div>
+      <a href='/welcome/login' className="absolute bottom-40 bg-opacity-90 hover:bg-opacity-100 cursor-pointer right-40 bg-hippiegreen text-white text-xl hover:text-2xl rounded-full p-4"><BsArrowRight /></a>
     </main>
-  )
-}
+  );
+};
+
+export default HelloPage;
