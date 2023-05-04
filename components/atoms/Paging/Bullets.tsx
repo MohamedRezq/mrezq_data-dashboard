033f2ea2cc17812395cc11602e972fd7d0e4432a
MohamedRezq <mrezq.dev@gmail.com>
2023-05-04 19:30:40 +0300

    fixed-1

--- components/atoms/Paging/Bullets.tsx
+++ components/atoms/Paging/Bullets.tsx
@@ -11,6 +11,7 @@ const Bullets = (props: BulletsProps) => {
       {Array.from({ length: props.count }, (_, i) => (
         <div
           className={`w-3 rounded-full h-3 bg-${props.active===i+1 ? "hippiegreen" : "alto"}`}
+          key={`bullet-key-${i}`}
         ></div>
       ))}
     </div>
