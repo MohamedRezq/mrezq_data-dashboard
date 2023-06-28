6b17f07daf4f6701258b6d72ec1ab24248e782c0
AmrHammdalla <amrhammdalla2017@gmail.com>
2023-06-28 13:03:50 +0200

    zoho-books-integration

--- app_config.ts
+++ app_config.ts
@@ -11,13 +11,23 @@ export const App_Config = {
       : process.env.NEXT_PUBLIC_API_BASE_URL_DEV,
 };
 
+//Scopes
+export const Scopes = {
+  ZP_SCOPE: "scope=ZOHOPEOPLE.employee.ALL",
+  ZB_SCOPE:
+    "scope=ZohoBooks.vendorpayments.READ,ZohoBooks.invoices.READ,ZohoBooks.customerpayments.READ,ZohoBooks.purchaseorders.READ",
+};
+
 // Integrations
 export const Integrations = {
   Quickbooks: {
     AUTH_URL: `${process.env.NEXT_PUBLIC_QUICKBOOKS_OAUTH_URL}?client_id=${process.env.NEXT_PUBLIC_QUICKBOOKS_CLIENT_ID}&response_type=code&scope=com.intuit.quickbooks.accounting&redirect_uri=${App_Config.APP_BASE_URL}${process.env.NEXT_PUBLIC_QUICKBOOKS_REDIRECT_URL}&state=security_token`,
   },
   Zohopeople: {
-    AUTH_URL: `${process.env.NEXT_PUBLIC_ZOHOPEOPLE_OAUTH_URL}scope=ZOHOPEOPLE.employee.ALL&client_id=${process.env.NEXT_PUBLIC_ZOHOPEOPLE_CLIENT_ID}&response_type=code&access_type=offline&prompt=consent&redirect_uri=${App_Config.APP_BASE_URL}${process.env.NEXT_PUBLIC_ZOHOPEOPLE_REDIRECT_URL}`,
+    AUTH_URL: `${process.env.NEXT_PUBLIC_ZOHO_OAUTH_URL}${Scopes.ZP_SCOPE}&client_id=${process.env.NEXT_PUBLIC_ZOHO_CLIENT_ID}&response_type=code&access_type=offline&prompt=consent&redirect_uri=${App_Config.APP_BASE_URL}${process.env.NEXT_PUBLIC_ZOHO_REDIRECT_URL}`,
+  },
+  Zohobooks: {
+    AUTH_URL: `${process.env.NEXT_PUBLIC_ZOHO_OAUTH_URL}${Scopes.ZB_SCOPE}&client_id=${process.env.NEXT_PUBLIC_ZOHO_CLIENT_ID}&response_type=code&access_type=offline&prompt=consent&redirect_uri=${App_Config.APP_BASE_URL}${process.env.NEXT_PUBLIC_ZOHO_REDIRECT_URL}`,
   },
 };
 
